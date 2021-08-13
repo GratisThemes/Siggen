3b496364fa9ad281edea34c82814fde4c3203a07
Kniffen <knifftech@gmail.com>
2021-08-13 14:16:31 +0200

    Fixed footer text setting not working

--- template-parts/site-footer.php
+++ template-parts/site-footer.php
@@ -29,7 +29,7 @@
     if ( '' !== $siggen_site_title ) {
       printf(
         '<span>%1$s %2$s %3$s</span>',
-        esc_html( $siggen_site_title ),
+        esc_html( get_theme_mod( 'footer_text', $siggen_site_title ) ),
         get_theme_mod( 'footer_copyright', true ) ? '&copy;' : '',
         get_theme_mod( 'footer_year', true ) ? gmdate( 'Y' ) : '' // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
       );
